29fd8269a2ae01a92712c76ec98594f9a48b5069
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-08-21 18:48:44 -0500

    refact: player use of ctx as param instead of being a property

--- app/scripts/characters/player.js
+++ app/scripts/characters/player.js
@@ -2,34 +2,34 @@ import Character from './character';
 import Bullet from '../env/bullet';
 
 export default class Player extends Character{
-  constructor(x, y, ctx) {
-    super(x, y, 50, 50, ctx, 20);
+  constructor(x, y) {
+    super(x, y, 50, 50, 20);
     this.bullets = [];
   }
-  draw () {
-    this.drawPlayer();
-    this.drawBullets();
+  draw (ctx) {
+    this.drawPlayer(ctx);
+    this.drawBullets(ctx);
   }
 
-  drawPlayer () {
-    this.ctx.save();
-    this.ctx.fillStyle = 'white';
-    this.ctx.fillRect(this.x, this.y, this.w, this.h);
-    this.ctx.restore();
+  drawPlayer (ctx) {
+    ctx.save();
+    ctx.fillStyle = 'white';
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+    ctx.restore();
   }
-  drawBullets() {
-    this.ctx.save();
+  drawBullets(ctx) {
+    ctx.save();
     for (let i in this.bullets) {
-      this.bullets[i].move(2);
-      this.bullets[i].draw();
+      this.bullets[i].move(5);
+      this.bullets[i].draw(ctx);
     };
-    this.ctx.restore();
+    ctx.restore();
     this.bullets = this.bullets.filter(function(filter) {
       return filter.y>0;
     });
   }
   fire() {
-    let Bull = new Bullet(this.x+(this.w*.5), this.y-(this.h*.5), this.ctx);
+    let Bull = new Bullet(this.x+(this.w*.5), this.y-(this.h*.5));
     this.bullets.push(Bull);
   }
   jump() {
